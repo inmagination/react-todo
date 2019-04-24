dae21616bd6f7922d4a8362e1605ad51d00e631d
inmaculada.reyes <inmaculada.reyes@softtek.com>
2019-04-24 11:08:04 +0200

    Updates

---
+++ precache-manifest.54e64ed391c2eb1d976e51b51d7bc4bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "d8fdce982657f47f8c9d",
+    "url": "/react-todo/static/js/main.69b43d3b.chunk.js"
+  },
+  {
+    "revision": "6c250bab5ea9da3e5506",
+    "url": "/react-todo/static/js/2.dd6a34b2.chunk.js"
+  },
+  {
+    "revision": "d8fdce982657f47f8c9d",
+    "url": "/react-todo/static/css/main.f67a645d.chunk.css"
+  },
+  {
+    "revision": "1917a024e9f62329928acf540ba17a02",
+    "url": "/react-todo/index.html"
+  }
+];
